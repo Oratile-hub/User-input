78fa1702642c11bcf665b0cf81af459666af745a
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-11 14:32:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Hi " + firstname);
+
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have " + age + " years");
+
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
